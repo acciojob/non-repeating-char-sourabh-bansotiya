3057a76b35fa896731dffd2c3b339e1259746f99
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-29 23:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++) {
+		for (let j = i+1; j < str.length; j++) {
+			if (str[i] != str[j) {
+				return str[i];
+			}
+			else{
+				return "null";
+			}
+		}
+	}
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
